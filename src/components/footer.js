565abeb7d4d48f9b895d29b9627458e766eef4fb
Pedro P <pedroaperezdr@gmail.com>
2023-11-12 14:55:06 -0500

    Updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,13 +8,13 @@ function Footer(){
             <a href="/" className="site-title">PAPDR</a>
             <ul>
                 <li>
-                    <a href="/projects">GitHub</a>
+                    <a href="https://github.com/PAPDR">GitHub</a>
                 </li>
                 <li>
-                    <a href="/about">LinkedIN</a>
+                    <a href="https://www.linkedin.com/feed/">LinkedIN</a>
                 </li>
                 <li>
-                    <a href="/clicker-game">Itch.io</a>
+                    <a href="https://itch.io/">Itch.io</a>
                 </li>
             </ul>
 
